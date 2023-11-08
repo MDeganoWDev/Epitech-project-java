60c99993d02d611f907e1498aaf3c92c581041c7
Matteo Degano <matteo.degano@epitech.eu>
2023-11-08 12:02:46 +0100

    start init

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import main.java.mvc.controller.GameController;
+import javax.swing.SwingUtilities;
+
+public class Application {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            GameController gameController = new GameController(10);
+            gameController.startNewGame();
+        });
+    }
+}
---
+++ src/main/java/mvc/controller/GameController.java
@@ -0,0 +1,69 @@
+package main.java.mvc.controller;
+
+import main.java.mvc.model.Board;
+import main.java.mvc.view.MainFrame;
+import javax.swing.*;
+
+public class GameController {
+    private Board board;
+    private MainFrame mainFrame;
+    private boolean isGameInProgress;
+
+    public GameController(int size) {
+        board = new Board(size);
+        mainFrame = new MainFrame(this);
+        isGameInProgress = false;
+    }
+    public Board getBoard() {
+        return board;
+    }
+    public void startNewGame() {
+        isGameInProgress = true;
+        board.resetBoard();
+        // TODO: Implement ship placement logic here, potentially with user input
+        if (!placeShip(2, 3, 4, true)) {
+            JOptionPane.showMessageDialog(mainFrame, "Failed to place ship at the start of the game.",
+                    "Ship Placement Error", JOptionPane.ERROR_MESSAGE);
+        }
+        mainFrame.repaintBoard();
+    }
+    public boolean placeShip(int row, int col, int length, boolean horizontal) {
+        if (!isGameInProgress) {
+            JOptionPane.showMessageDialog(mainFrame, "Can't place ships until the game starts.",
+                    "Game Not In Progress", JOptionPane.WARNING_MESSAGE);
+            return false;
+        }
+        if (board.placeShip(row, col, length, horizontal)) {
+            mainFrame.repaintBoard();
+            return true;
+        } else {
+            JOptionPane.showMessageDialog(mainFrame, "Can't place a ship there.",
+                    "Invalid Ship Placement", JOptionPane.WARNING_MESSAGE);
+            return false;
+        }
+    }
+
+    public void takeShot(int row, int col) {
+        if (!isGameInProgress) {
+            JOptionPane.showMessageDialog(mainFrame, "Can't take shots until the game starts.",
+                    "Game Not In Progress", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        if (board.getCellStatus(row, col) == Board.Status.EMPTY ||
+                board.getCellStatus(row, col) == Board.Status.SHIP) {
+            board.takeShot(row, col);
+            if (areAllShipsSunk()) {
+                JOptionPane.showMessageDialog(mainFrame, "All ships have been sunk! Game over.",
+                        "Game Over", JOptionPane.INFORMATION_MESSAGE);
+                isGameInProgress = false;
+            }
+            mainFrame.repaintBoard();
+        } else {
+            JOptionPane.showMessageDialog(mainFrame, "You've already shot there.",
+                    "Invalid Shot", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+    public boolean areAllShipsSunk() {
+        return board.areAllShipsSunk();
+    }
+}
